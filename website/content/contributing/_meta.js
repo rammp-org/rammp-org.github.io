4394b1cf7da533ade9b74cf98ed2bf7e23a652df
ewang360 <elw96@pitt.edu>
2026-09-01 18:42:31 -0400

    started contribution section

---
+++ website/content/contributing/_meta.js
@@ -0,0 +1,7 @@
+export default {
+  index: 'Overview',
+  'new-repo': 'Add a new repo',
+  'publishing-docs': 'Publish its docs here',
+  'how-to': 'How-to guide',
+  checklist: 'Before you open the PR'
+}
