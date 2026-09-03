b88dc89f1073b2ae16b2fcacfee61f81ac2a1fe7
ewang360 <elw96@pitt.edu>
2026-09-03 15:38:21 -0400

    corrected installation instructions

--- website/content/contributing/_meta.js
+++ website/content/contributing/_meta.js
@@ -1,7 +1,6 @@
 export default {
   index: 'Overview',
-  'new-repo': 'Add a new repo',
-  'publishing-docs': 'Publish its docs here',
   'how-to': 'How-to guide',
+  'rammp-interfaces': 'RAMMP-interfaces spec',
   checklist: 'Before you open the PR'
 }
