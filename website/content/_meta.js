27663148d60994629ce756df1131f7010bd255f1
ewang360 <elw96@pitt.edu>
2026-09-09 20:52:36 -0400

    fleshed out intro pages

--- website/content/_meta.js
+++ website/content/_meta.js
@@ -2,8 +2,6 @@ export default {
   index: 'Introduction',
   platform: 'The platform',
   interfaces: 'Interfaces',
-  building: 'Build a module',
-  running: 'Run the system',
   sheppy: 'sheppy',
   'kinova-gen3-ros2': 'Controlling the arm'
 }
